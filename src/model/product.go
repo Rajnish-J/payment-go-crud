7ef8d5d0adc1926818109499e915e464783cc35b
Saikeerthana@91 <saikeerthanakothapelli@gmail.com>
2025-06-15 23:30:08 +0530

    fix: Added relationship between company and product

--- src/model/product.go
+++ src/model/product.go
@@ -4,5 +4,6 @@ type Product struct {
 	ID        uint `gorm:"primaryKey"`
 	Name      string
 	CompanyID uint
+	Company   Company `gorm:"foreignKey:CompanyID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Price     float64
 }
