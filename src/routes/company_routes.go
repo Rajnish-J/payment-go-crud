1601c7d137386a4635a35a0ab5dbf4885b090f87
Saikeerthana@91 <saikeerthanakothapelli@gmail.com>
2025-06-17 17:05:11 +0530

    feat: Implement CRUD operations for company management and update Swagger documentation

--- src/routes/company_routes.go
+++ src/routes/company_routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"crud/src/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,8 +11,8 @@ func RegisterCompanyRoutes(router *gin.Engine) {
 	{
 		api.POST("/company", controller.CreateCompany)
 		api.GET("/company", controller.GetCompanies)
-		api.GET("/company:id", controller.GetCompanyByID)
+		api.GET("/company/:id", controller.GetCompanyByID)
 		api.PUT("/company", controller.UpdateCompany)
-		api.DELETE("/company:id", controller.DeleteCompany)
+		api.DELETE("/company/:id", controller.DeleteCompany)
 	}
 }
