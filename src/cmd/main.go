ec338ac93e1f7b94a1e9c324d18cabb995478fe9
Saikeerthana@91 <saikeerthanakothapelli@gmail.com>
2025-06-17 17:57:39 +0530

    REFAC: Organize import statements in main.go

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -7,13 +7,13 @@
 package main
 
 import (
+	_ "crud/docs"
+	"crud/src/config"
+	"crud/src/routes"
+
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-
-
-	"crud/src/config"
-	"crud/src/routes"
 )
 
 func main() {
